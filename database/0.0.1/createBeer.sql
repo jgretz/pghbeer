487987ca0205f982ad746de7d2b8097a8817699f
Josh Gretz <jgretz@truefit.io>
2019-08-05 09:50:46 -0500

    update beer to have more info

--- database/0.0.1/createBeer.sql
+++ database/0.0.1/createBeer.sql
@@ -3,5 +3,9 @@ create table beer
   id int PRIMARY KEY,
   breweryId int not null,
   name varchar(50) not null,
+  description varchar(255),
+  style varchar(50),
+  abv decimal,
+
   active bit not null,
 )
