1a53da1d585afe1d3a8248cd26c4b3e34904d75c
Josh Gretz <jgretz@truefit.io>
2019-10-08 15:05:05 -0400

    update

---
+++ site/app/features/landing/components/landing.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (
+  <div>Beers of the Burgh Winter Warmer list coming soon !!!</div>
+);
--- site/app/features/shared/components/routes.js
+++ site/app/features/shared/components/routes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Switch, Route} from 'react-router';
 
+import {Landing} from '../../landing/components';
 import NotFound from './notFound';
 
 export default () => (
   <Switch>
+    <Route path="/" exact component={Landing} />
     <Route component={NotFound} />
   </Switch>
 );
