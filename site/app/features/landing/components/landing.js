2ca40cad8bd7ec576e5d2f848bd876b3b70dd3d8
Josh Gretz <jgretz@truefit.io>
2019-10-30 11:44:14 -0400

    fix hostname

--- site/app/features/landing/components/landing.js
+++ site/app/features/landing/components/landing.js
@@ -5,7 +5,7 @@ import {Checklist} from '../../checklist/components';
 import Welcome from './welcome';
 
 const isBotbList = () => {
-  return window.location.hostname === 'list.beersoftheburgh';
+  return window.location.hostname === 'list.beersoftheburgh.com';
 };
 
 export default compose(renderIf(isBotbList, Checklist))(Welcome);
