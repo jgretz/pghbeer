dbb703b5a8d63d1926d3aaabdd318900a66d906a
Josh Gretz <jgretz@truefit.io>
2021-08-21 13:50:19 -0400

    update build

--- tools/build.js
+++ tools/build.js
@@ -1,42 +1,38 @@
 /* eslint-disable no-var */
-require('shelljs/global');
+require("shelljs/global");
 
 // clean
-echo('Cleaning ...');
-rm('-rf', './lib');
-mkdir('./lib');
+echo("Cleaning ...");
+rm("-rf", "./lib");
+mkdir("./lib");
 
 // move over package.json
-echo('Moving package.json ...');
+echo("Moving package.json ...");
 
-cp('./package.prod.json', './lib/package.json');
+cp("./package.prod.json", "./lib/package.json");
 
 // build app
-echo('Building Server ...');
-cd('api');
-exec('yarn', () => {
-  exec('yarn build', () => {
-    cd('../lib');
-    
-    mv('../api/lib/*', './');
-    rm('-rf', '../api/lib');
-    rm('constants/private.js');
-  
+echo("Building Server ...");
+cd("../api");
+exec("yarn", () => {
+  exec("yarn build", () => {
+    cd("../tools/lib");
+
+    mv("../../api/lib/*", "./");
+    rm("-rf", "../../api/lib");
+
     // build site
-    echo('Building Site ...');
+    echo("Building Site ...");
 
     // build admin spa and move it
-    cd('../site');
-    exec('yarn', () => {
-      exec('yarn build', () => {
-        cd('..');
-        mv('./site/lib', 'lib/site');
-      
-        echo('Build Complete');
+    cd("../../site");
+    exec("yarn", () => {
+      exec("yarn build", () => {
+        cd("../tools/lib");
+        mv("../../site/lib", "./site");
+
+        echo("Build Complete");
       });
     });
   });
 });
-
-
-
