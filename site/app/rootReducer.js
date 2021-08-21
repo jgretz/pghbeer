2225594c5f251bb112a30dbe70f9a34d6d496cd1
Josh Gretz <jgretz@truefit.io>
2021-08-21 14:12:16 -0400

    update deploy stuff

--- site/app/rootReducer.js
+++ site/app/rootReducer.js
@@ -3,27 +3,27 @@
 /* eslint-disable object-shorthand */
 import {combineReducers} from 'redux';
 import {connectRouter} from 'connected-react-router';
-import breweries from './features/breweries/reducers';
 import beers from './features/beers/reducers';
+import breweries from './features/breweries/reducers';
 import checklist from './features/checklist/reducers';
 import events from './features/events/reducers';
 import navigation from './features/navigation/reducers';
-import stats from './features/stats/reducers';
 import shared from './features/shared/reducers';
 import styles from './features/styles/reducers';
+import stats from './features/stats/reducers';
 import users from './features/users/reducers';
 
 const rootReducer = history =>
   combineReducers({
     features: combineReducers({
-      breweries: breweries,
       beers: beers,
+      breweries: breweries,
       checklist: checklist,
       events: events,
       navigation: navigation,
-      stats: stats,
       shared: shared,
       styles: styles,
+      stats: stats,
       users: users,
     }),
     router: connectRouter(history),
