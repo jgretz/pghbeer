4a1a5f7c77567dfce43e235d45afa2b7f9470baa
Josh Gretz <jgretz@truefit.io>
2024-06-04 20:58:41 -0400

    update data (06/04/24)

--- tools/import/src/index.ts
+++ tools/import/src/index.ts
@@ -1,5 +1,5 @@
 import {PrismaClient, beers, breweries, styles} from '@prisma/client';
-import * as data from './data/2024/load5.json';
+import * as data from './data/2024/load6.json';
 
 interface LoadData {
   brewery: string;
