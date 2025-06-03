a452bdf3058bc9f3b54cd585ddf722686689efe5
Josh Gretz <jgretz@gmail.com>
2025-06-03 11:18:21 -0400

    update data for 6-3-25

--- tools/import/src/index.ts
+++ tools/import/src/index.ts
@@ -1,5 +1,5 @@
 import {PrismaClient, beers, breweries, styles} from '@prisma/client';
-import * as data from './data/2025/load1.json';
+import * as data from './data/2025/load2.json';
 
 interface LoadData {
   brewery: string;
